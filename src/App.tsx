6724201b2ff62d2f95b53f7bcadfc6334cde38d2
Joki <josedavidamayar@gmail.com>
2025-02-16 19:22:00 -0500

    feat: made the app the main component where the states of the zones and sidebar change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import { ThreeScene } from "./ThreeScene";
+import { useState } from "react";
+import { Zone } from "./types/Zone";
+import Main from "./components/Main";
+import { SideBar } from "./components/SideBar";
+import { universityData } from "./data/universityData";
 
 function App() {
+  const [data, setData] = useState<Zone | null>(null);
+  const [showModal, setShowModal] = useState(false);
+
+  /**
+   * handle the showModal value
+   */
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  /**
+   * Update data based on the selected zone ID
+   */
+  const handleZoneSelection = (zoneId: string) => {
+    const selectedZone =
+      universityData.find((zone) => zone.id === zoneId) || null;
+    setData(selectedZone);
+  };
+
   return (
-    <div className="w-full h-screen">
-      <ThreeScene />
-    </div>
+    <>
+      <Main handleZoneSelection={handleZoneSelection} />
+
+      {/* Button to open the sidebar */}
+      <button
+        onClick={handleToggleModal}
+        className="mt-4 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+      >
+        Open Sidebar
+      </button>
+
+      {showModal && (
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
+      )}
+    </>
   );
 }
 
