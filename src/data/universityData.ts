8664390b73d64c333ebfda5d59715cb25e035042
Joki <josedavidamayar@gmail.com>
2025-02-16 19:20:04 -0500

    feat: saved two zones information

---
+++ src/data/universityData.ts
@@ -0,0 +1,173 @@
+import { Zone } from "../types/Zone";
+
+export const universityData: Zone[] = [
+  {
+    id: "guadales",
+    name: "Guaduales",
+    buildings: [
+      {
+        id: "b1",
+        name: "Facultad de Ciencias Básicas, Humanas y Bellas Artes",
+        type: "Facultad",
+        description:
+          "Facultad de Ciencias Básicas, Humanas y Bellas Artes, con laboratorios y aulas especializadas",
+        coordinates: { x: 10, y: 20, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b2",
+        name: "Auditorio Bernardo Ramirez",
+        type: "Auditorio",
+        description:
+          "Auditorio principal de la facultad de Ciencias para eventos académicos.",
+        coordinates: { x: 15, y: 22, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b3",
+        name: "Biblioteca Infantil",
+        type: "Biblioteca",
+        description: "Espacio para el aprendizaje infantil.",
+        coordinates: { x: 12, y: 25, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b4",
+        name: "Cepas",
+        type: "Instituto",
+        description: "Centro de estudios y proyectos avanzados.",
+        coordinates: { x: 18, y: 26, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b5",
+        name: "Instituto interdisciplinario de ciencias",
+        type: "Instituto",
+        description: "Centro de estudios y proyectos de ciencias",
+        coordinates: { x: 18, y: 26, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b6",
+        name: "Edificio 50 años",
+        type: "Edificio",
+        description:
+          "Construido para conmemorar el cincuentenario de la institución, este edificio alberga múltiples aulas diseñadas para la enseñanza de diversas disciplinas.",
+        coordinates: { x: 18, y: 26, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b7",
+        name: "Laboratorio de Suelos",
+        type: "Facultad",
+        description: "Laboratorio especializado en análisis de suelos.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b8",
+        name: "Laboratorio de Bioterio",
+        type: "Facultad",
+        description: "Laboratorio especializado para fines medicos.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b9",
+        name: "Jardin Botanico",
+        type: "Vivero",
+        description:
+          "Este jardín alberga una gran variedad de especies de flora nativa y exótica, proporcionando un entorno ideal para el estudio de la biodiversidad y la ecología.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b11",
+        name: "Finca la Aldana",
+        type: "Laboratorio",
+        description:
+          "Ubicada dentro del campus, esta finca funciona como un laboratorio al aire libre donde los estudiantes pueden desarrollar proyectos relacionados con la agricultura, la zootecnia, la agroindustria y la sostenibilidad.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b12",
+        name: "Centro de Acopio",
+        type: "Laboratorio",
+        description:
+          "Es una instalación destinada al almacenamiento, clasificación y gestión de materiales agrícolas y de investigación.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "b13",
+        name: "Lombricultivo y Lombricultura",
+        type: "Vivero",
+        description:
+          "Espacio dedicado al estudio y aplicación de técnicas de compostaje y producción de abonos orgánicos a través de la cría de lombrices.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+    ],
+  },
+  {
+    id: "cambulus",
+    name: "Cambulos",
+    buildings: [
+      {
+        id: "c1",
+        name: "Coliseo",
+        type: "Coliseo",
+        description:
+          "Es el principal escenario deportivo del campus, diseñado para la práctica y desarrollo de diversas disciplinas atléticas.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "c2",
+        name: "Bienestar Institucional",
+        type: "Edificio",
+        description:
+          "Es un espacio dedicado al desarrollo integral de los estudiantes, docentes y administrativos, ofreciendo programas y servicios que promueven el bienestar físico, emocional y social.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "c4",
+        name: "Canchas Multiples",
+        type: "Cancha",
+        description:
+          "Son espacios deportivos diseñados para la práctica de diversas disciplinas como baloncesto, voleibol y fútbol sala.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "c7",
+        name: "Cancha de fútbol",
+        type: "Cancha",
+        description:
+          "Es un espacio amplio y adecuado para la práctica y competencia de este deporte.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "c9",
+        name: "Teatrino y Media Torta",
+        type: "Coliseo",
+        description:
+          "Es un escenario al aire libre diseñado para la realización de eventos culturales, artísticos y académicos.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+      {
+        id: "c10",
+        name: "Centro de Salud",
+        type: "Centro de Salud",
+        description:
+          "Es una unidad dedicada a la atención médica primaria y el bienestar de la comunidad universitaria.",
+        coordinates: { x: 21, y: 30, z: 0 },
+        pointsOfInterest: [],
+      },
+    ],
+  },
+];
