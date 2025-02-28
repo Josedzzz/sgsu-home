efa1ba1b71787d3ad7c9e448555cfcc08d53d237
Joki <josedavidamayar@gmail.com>
2025-02-28 12:44:24 -0500

    style: new style and animation effects for the building info and component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -14,7 +14,7 @@ export function SideBar({
   buildingId,
 }: SideBarProps) {
   return (
-    <div className="flex flex-col gap-4 p-6 bg-white text-black shadow-lg rounded-lg h-auto max-h-[calc(100vh-6rem)] md:overflow-y-auto">
+    <div className="flex flex-col gap-4 p-6 bg-white text-black shadow-lg rounded-lg h-auto max-h-[calc(100vh-6rem)] overflow-y-auto animate-[fade-in_0.8s_ease-in]">
       {data ? (
         <>
           <h2 className="text-2xl font-bold border-b pb-2 border-gray-300">
@@ -25,9 +25,9 @@ export function SideBar({
             {data.buildings.map((building: Building) => (
               <div
                 key={building.id}
-                className={`p-4 rounded-lg shadow ${
+                className={`p-4 rounded-lg shadow hover:scale-105 transition-transform duration-300 animate-[slide-in-right_0.7s_ease-in] ${
                   building.id === buildingId
-                    ? "bg-emerald-100 border-2 border-emerald-500"
+                    ? "bg-emerald-100 border-2 border-emerald-500 scale-105"
                     : "bg-gray-100"
                 }`}
                 onClick={() => handleBuildSelection(building.id)}
